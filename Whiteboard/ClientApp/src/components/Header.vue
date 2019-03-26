1cbdfc9f427761bfd3e385956fb3a44a592696c4
cantataMajora <fufulomoffu@gmail.com>
2019-03-26 23:04:30 +0300

    Test header created

---
+++ Whiteboard/ClientApp/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <header>
+        <h1> This is header!</h1>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        background: lightgreen;
+        padding: 10px;
+    }
+
+</style>
--- Whiteboard/ClientApp/src/views/Home.vue
+++ Whiteboard/ClientApp/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <UserHeader/>
+    <!--HelloWorld msg="Welcome to Your Vue.js App" -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import HelloWorld from '@/components/HelloWorld.vue'
+    import UserHeader from '@/components/Header.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+      UserHeader
   }
 }
 </script>

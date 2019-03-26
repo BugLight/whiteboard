f3c4240f2785bde079343b633e78f802464fbe64
cantataMajora <fufulomoffu@gmail.com>
2019-03-26 23:26:34 +0300

    Link setup + style changes

--- Whiteboard/ClientApp/src/components/Header.vue
+++ Whiteboard/ClientApp/src/components/Header.vue
@@ -1,19 +1,19 @@
 <template>
     <header>
-        <h1> Whiteboard </h1>
+        <router-link to="/"><h1>Whiteboard</h1></router-link>
     </header>
 </template>
 
 <style scoped lang="stylus">
     header {
-        background: lightgreen;
+        background-color:lightcyan;
         padding: 10px;
     }
 
     h1 {
         color: #000000;
         text-align: left;
-        font-family:'Franklin Gothic';
+        font-family:'Franklin Gothic', 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
         font-style: italic;
     }
 

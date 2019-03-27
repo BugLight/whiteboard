6ccb167a7f7510d0d29f7a391a55150865f9e055
buglight <buglight@kistriver.com>
2019-03-27 19:57:00 +0300

    Rooms for connections

---
+++ Whiteboard.Test/TestActiveRoom.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+
+namespace Whiteboard.Test
+{
+    [TestFixture]
+    public class TestActiveRoom
+    {
+        private Connection connection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            connection = new Connection("");
+        }
+
+        [Test]
+        public void TestInitialCount()
+        {
+            var room = new ActiveRoom();
+            Assert.That(room.ConnectionsCount == 0);
+        }
+
+        [Test]
+        public void TestIncCount()
+        {
+            var room = new ActiveRoom
+            {
+                MaxConnections = 1
+            };
+            var count = room.ConnectionsCount;
+            room.Join(connection);
+            Assert.That(room.ConnectionsCount == count + 1);
+        }
+
+        [Test]
+        public void TestDecCount()
+        {
+            var room = new ActiveRoom
+            {
+                MaxConnections = 1
+            };
+            var count = room.ConnectionsCount;
+            room.Join(connection);
+            room.Leave(connection);
+            Assert.That(room.ConnectionsCount == count);
+        }
+
+        [Test]
+        public void TestEmptyRoom()
+        {
+            var room = new ActiveRoom();
+            Assert.That(() => room.Leave(connection), Throws.Exception);
+        }
+
+        [Test]
+        public void TestFullRoom()
+        {
+            var room = new ActiveRoom
+            {
+                MaxConnections = 1
+            };
+            for (int i = 0; i < room.MaxConnections; i++)
+                room.Join(new Connection(""));
+            Assert.That(() => room.Join(connection), Throws.Exception);
+        }
+    }
+}
---
+++ Whiteboard/ActiveRoom.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using Whiteboard.Models;
+
+namespace Whiteboard
+{
+    public class ActiveRoom : Room
+    {
+        public int ConnectionsCount { get; private set; }
+        private readonly Mutex mutex = new Mutex(); 
+
+        public event EventHandler<Connection> OnJoined;
+        public event EventHandler<Connection> OnLeft;
+
+        public void Join(Connection connection)
+        {
+            mutex.WaitOne();
+            if (ConnectionsCount == MaxConnections)
+                throw new Exception();
+            ConnectionsCount++;
+            mutex.ReleaseMutex();
+            connection.Room = this;
+            connection.OnClosed += (sender, args) =>
+            {
+                Leave((Connection)sender);
+            };
+            OnJoined?.Invoke(this, connection);
+        }
+
+        public void Leave(Connection connection)
+        {
+            mutex.WaitOne();
+            if (ConnectionsCount == 0)
+                throw new Exception();
+            ConnectionsCount--;
+            mutex.ReleaseMutex();
+            connection.Room = null;
+            OnLeft?.Invoke(this, connection);
+        }
+    }
+}

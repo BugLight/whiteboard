20dd3ca50ebf03eff6003a61fa23523976dcac41
mesuchan <seminanele@gmail.com>
2019-03-27 15:07:57 +0300

    Context was added.

---
+++ Whiteboard/AppContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Whiteboard.Models;
+
+namespace Whiteboard
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Room> Rooms { get; set; }
+
+        public AppContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}

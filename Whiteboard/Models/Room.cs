5550ae57921fb58c7367e2747dddca0beb1f5475
mesuchan <seminanele@gmail.com>
2019-03-27 15:00:26 +0300

    Model room was added.

---
+++ Whiteboard/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Whiteboard.Models
+{
+    public class Room
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+
+        // Primary key
+        public Guid Id { get; set; }
+
+        // Name of room
+        public string Name { get; set; }
+        // Number of maximum amount of connections
+        public int MaxConnections { get; set; }
+    }
+}

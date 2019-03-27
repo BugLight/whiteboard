98098eed009b760dd477334ec0503eb1bd331c30
cantataMajora <fufulomoffu@gmail.com>
2019-03-27 15:45:16 +0300

    Removing unnecessary links

--- Whiteboard/ClientApp/src/App.vue
+++ Whiteboard/ClientApp/src/App.vue
@@ -8,11 +8,6 @@
         </navigation>
 
         <router-view />
-        <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link>
-        </div>
-
     </div>
 </template>
 <script>
@@ -36,12 +31,4 @@ html, body
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
-
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
 </style>

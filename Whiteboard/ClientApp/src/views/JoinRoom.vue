69b92daf1144890038fa77ebb2cb721913d8e16e
Dan <buglight@kistriver.com>
2019-03-27 18:22:21 +0300

    Update JoinRoom.vue

--- Whiteboard/ClientApp/src/views/JoinRoom.vue
+++ Whiteboard/ClientApp/src/views/JoinRoom.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="join_room">
-        <h1>Присоединение к комнате</h1>
-        <form class="form">
-            <div class="form-control">
-                <label class="form-control__label">id комнаты</label>
-                <input type="text" />
-            </div>
-            <div class="form-control">
-                <button class="button button_form button_submit">Подключиться</button>
-            </div>
-        </form>
-    </div>
+  <div class="join_room">
+    <h1>РџСЂРёСЃРѕРµРґРёРЅРёС‚СЊСЃСЏ Рє РєРѕРјРЅР°С‚Рµ</h1>
+    <form class="form">
+      <div class="form-control">
+        <label class="form-control__label">id РєРѕРјРЅР°С‚С‹</label>
+        <input type="text"/>
+      </div>
+      <div class="form-control">
+        <button class="button button_form button_submit">РџСЂРёСЃРѕРµРґРёРЅРёС‚СЊСЃСЏ</button>
+      </div>
+    </form>
+  </div>
 </template>

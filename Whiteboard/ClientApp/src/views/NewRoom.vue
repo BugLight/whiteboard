95905f51eafacc91885b875b1eff3400642f0acb
buglight <buglight@kistriver.com>
2019-03-27 17:30:59 +0300

    Added view-model to form

--- Whiteboard/ClientApp/src/views/NewRoom.vue
+++ Whiteboard/ClientApp/src/views/NewRoom.vue
@@ -4,11 +4,11 @@
     <form class="form">
       <div class="form-control">
         <label class="form-control__label">Название</label>
-        <input type="text"/>
+        <input type="text" v-model="room.name"/>
       </div>
       <div class="form-control">
         <label class="form-control__label">Максимум пользователей</label>
-        <input type="number"/>
+        <input type="number" min="1" v-model="room.maxConnections"/>
       </div>
       <div class="form-control">
         <button class="button button_form button_submit">Создать</button>
@@ -16,6 +16,19 @@
     </form>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        room: {
+          name: '',
+          maxConnections: 1
+        }
+      };
+    }
+  }
+</script>
 
 <style lang="stylus">
   control-radius = 3px

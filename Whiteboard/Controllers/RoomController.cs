fea67b5fc19a0b952302299a1e85cd52bc6d3b14
mesuchan <seminanele@gmail.com>
2019-03-27 16:53:52 +0300

    RoomController was added.

---
+++ Whiteboard/Controllers/RoomController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Whiteboard.Models;
+
+namespace Whiteboard.Controllers
+{
+    [Route("api/rooms")]
+    public class RoomController : Controller
+    {
+        private readonly AppContext context;
+
+        public RoomController(AppContext context)
+        {
+            this.context = context;
+        }
+
+        // Function for adding room
+        [HttpPost("meals")]
+        public ActionResult<Room> AddMeal([FromBody] Room room)
+        {
+            if (room == null)
+                return BadRequest();
+
+            if (string.IsNullOrEmpty(room.Name))
+                return BadRequest();
+
+            if (room.MaxConnections > 0)
+                return BadRequest();
+
+            context.Rooms.Add(room);
+            context.SaveChanges();
+
+            return room;
+        }
+    }
+}

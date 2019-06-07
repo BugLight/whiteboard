3b96900eafe742d4e0107045dd84344fa8e457ee
cantataMajora <fufulomoffu@gmail.com>
2019-06-07 17:51:17 +0300

    Using configurated json

--- Whiteboard/Startup.cs
+++ Whiteboard/Startup.cs
@@ -27,7 +27,7 @@ namespace Whiteboard
 
             services.AddSignalR();
 
-            string connectionString = @"Data Source = DESKTOP-L2AHU82; Initial Catalog = Whiteboard; Integrated Security = true";
+            string connectionString = Configuration["ConnectionString:DefaultConnection"];
             services.AddDbContext<AppContext>(options => options.UseSqlServer(connectionString));
 
             var builder = new DbContextOptionsBuilder<AppContext>();
